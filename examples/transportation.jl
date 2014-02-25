ef546f4a6a24ba7f58301299a42e290e9eaef4c1
Joey Huchette <joehuchette@gmail.com>
2014-02-24 20:15:55 -0500

    remove leftover Block type; added transportation example from Kalvelagen

---
+++ examples/transportation.jl
@@ -0,0 +1,51 @@
+using JuMPStoch
+
+m = StochasticModel()
+
+factories = 1:3
+centers = 1:5
+scenarios = ["lo", "mid", "hi"]
+
+cap = [500, 450, 650]
+
+demand = [150 160 170;
+          100 120 135;
+          250 270 300;
+          300 325 350;
+          600 700 800]
+
+prob = [0.25, 0.5, 0.25]
+
+transcost = [2.49 5.21 3.76 4.85 2.07;
+             1.46 2.54 1.83 1.86 4.76;
+             3.26 3.08 2.60 3.76 4.45]
+
+prodcost = 14
+price = 24
+wastecost = 4
+
+@defVar(m, ship[factories,centers] >= 0)
+@defVar(m, product[factories] >= 0)
+@defVar(m, sales[centers] >= 0)
+@defVar(m, waste[centers] >= 0)
+@defVar(m, profit)
+
+@defVar(m, received[centers] >= 0)
+@setObjective(m, Max, -sum{transcost[i,j]*ship[i,j], i=factories, j=centers} + sum{prodcost*product[i], i=factories})
+for j in centers
+    @addConstraint(m, received[j] == sum{ship[i,j], i=factories})
+end
+for i in factories
+    @addConstraint(m, product[i] == sum{ship[i,j], j=centers})
+end
+
+for (s, elem) in enumerate(scenarios)
+    bl = StochasticBlock(m, s)
+    @defVar(bl, 0 <= salesw[i=centers] <= demand[i,s])
+    @defVar(bl, wastew[centers] >= 0)
+    @setObjective(bl, Max, sum{price*prob[s]*salesw[j], j=centers} - sum{wastecost*prob[s]*wastew[j], j=centers})
+    for j in centers
+        @addConstraint(bl, received[j] == salesw[j]+wastew[j])
+    end
+end
+
