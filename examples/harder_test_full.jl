d8c62d14b6febfa3c3223d63cf44b5b64288ce9e
Joey Huchette <joehuchette@gmail.com>
2014-06-17 11:19:29 -0500

    data dump

--- examples/harder_test_full.jl
+++ examples/harder_test_full.jl
@@ -12,9 +12,10 @@ m = Model()
 @addConstraint(m, w[1] - x - y <= 1)
 @addConstraint(m, w[1] + x == 2)
 
-@addConstraint(m, 2w[2] - x - y <= 5)
-@addConstraint(m, 3w[2] + x == 4)
+# @addConstraint(m, 2w[2] - x - y <= 5)
+# @addConstraint(m, 3w[2] + x == 4)
 
-setObjective(m, :Min, x*x + 0.5x*y + 0.25y*y - y + w[1]*w[1] + w[1] + 0.5w[2]*w[2] + 0.75w[2])
+# setObjective(m, :Min, x*x + 0.5x*y + 0.25y*y - y + w[1]*w[1] + w[1] + 0.5w[2]*w[2] + 0.75w[2])
+setObjective(m, :Min, x*x + 0.5x*y + 0.25y*y - y + w[1]*w[1] + w[1])
 
 solve(m)
