eb4260fff4a62bbf9b8380b005f138ffdd4a3524
Joey Huchette <joehuchette@gmail.com>
2014-06-18 16:39:29 -0500

    whoops

--- examples/large_simple_test.jl
+++ examples/large_simple_test.jl
@@ -14,7 +14,7 @@ m = StochasticModel()
 
 bl = StochasticBlock(m)
 @defVar(m, y[1:N] >= 0)
-@addConstraint(bl, r=1:N, x[r] + x[r] >= 1/2)
+@addConstraint(bl, constr[r=1:N], x[r] + x[r] >= 1/2)
 @setObjective(bl, Min, sum{y[i], i=1:N})
 
 StochJuMP.pips_solve(m)
