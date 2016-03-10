6a29291faf0c87b371bf19741936b4be7c7d0cb9
Feng Qiang <f.qiang@gmail.com>
2016-03-09 22:27:49 -0600

    remove the @show message

--- src/nlp.jl
+++ src/nlp.jl
@@ -1,13 +1,5 @@
 # Add parent::Int here to avoid overwritting same method in JuMP. Should be safe.
 function JuMP.parseNLExpr_runtime(m::JuMP.Model, x::JuMP.Variable, tape, parent::Int, values)
-    # @show m
-    # @show parent
-    # @show JuMP.getName(x)
-    # @show x.col
-    # @show tape
-    # @show parent
-    # @show values
-
     JuMP.__last_model[1] = x.m
     if x.m === m
         push!(tape, JuMP.NodeData(ReverseDiffSparse.VARIABLE, x.col, parent))
@@ -18,7 +10,7 @@ function JuMP.parseNLExpr_runtime(m::JuMP.Model, x::JuMP.Variable, tape, parent:
         else
             newx = JuMP.Variable(m,JuMP.getLower(x),JuMP.getUpper(x),:Cont, JuMP.getName(x))
             othermap[x] = newx  #parent -> dummy parent in the child node
-            @show x.col, "-->",newx.col
+            #@show x.col, "-->",newx.col
         end
         push!(tape, JuMP.NodeData(ReverseDiffSparse.VARIABLE, newx.col, parent))
     end
