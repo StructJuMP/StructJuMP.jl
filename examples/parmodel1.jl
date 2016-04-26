9a173fe76c5a0f5ed649f2fec5e3f2b13e32df2a
Feng Qiang <f.qiang@gmail.com>
2016-04-26 16:00:26 -0500

    correct parmodel1.jl

--- examples/parmodel1.jl
+++ examples/parmodel1.jl
@@ -1,15 +1,20 @@
+include("../src/pips_structure_interface.jl")
+#an example model
+
+using ParPipsInterface
 using StructJuMP, JuMP
 
-firststage = StructuredModel()
+scen = 2
+firststage = StructuredModel(num_scenarios=scen)
 @defVar(firststage, x[1:2])
 @setNLObjective(firststage, Min, (x[1]+x[2])^2)
 @addNLConstraint(firststage, x[1] * x[2] == 10)
 
-for scen in 1:2
+for i in 1:scen
     bl = StructuredModel(parent=firststage)
     @defVar(bl, y)
-    @addConstraint(bl, x[2]^2 + x[1]*y ≤ 5)
+    @addNLConstraint(bl, x[2]^2 + x[1]*y ≤ 5)
     @setNLObjective(bl, Min, (x[1]+x[2])*y)
 end
 
-solve(firststage)
+ParPipsInterface.solve(firststage)
