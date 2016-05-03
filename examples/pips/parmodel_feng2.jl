cb36c46f558f85a34549b8ed37a805272a4bb060
Feng Qiang <f.qiang@gmail.com>
2016-05-03 04:49:01 -0500

    update my example 2

--- examples/pips/parmodel_feng2.jl
+++ examples/pips/parmodel_feng2.jl
@@ -3,20 +3,20 @@ include("../../src/pips_structure_interface.jl")
 
 using ParPipsInterface
 
-using StochJuMP, JuMP
+using StructJuMP, JuMP
 
 #############
 # A sample model
 #############
 scen = 2
-m = StochasticModel(num_scenarios=scen)
+m = StructuredModel(num_scenarios=scen)
 @defVar(m, x[1:2])
 @defVar(m, z[1:2])
 @setNLObjective(m, Min, x[1]^2+x[2]^2+x[1]*z[2])
-@addNLConstraint(m, x[1] * x[2] <= 100)
+@addNLConstraint(m, x[1] * x[2]<= 100)
 
 for i in 1:scen
-    bl = StochasticModel(parent=m)
+    bl = StructuredModel(parent=m)
     @defVar(bl, y[1:2])
     @addNLConstraint(bl, x[2]*x[1] + x[1]*y[1]*y[2] == 10)
     @addNLConstraint(bl, x[2]*x[1]*y[1]*y[2] <= 100)
