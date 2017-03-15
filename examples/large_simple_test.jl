796d42c359f95c2724bd2630a693265ee07a551c
Kyle M. Tarplee <kmtarplee@anderson.edu>
2017-03-15 12:53:43 -0400

    added some smoke tests for a few examples
    The examples still need a lot of work to actually cycle but they are getting closer.

--- examples/large_simple_test.jl
+++ examples/large_simple_test.jl
@@ -3,13 +3,13 @@ using StructJuMP
 N = 1000
 numScen = 1
 
-m = StructuredModel(numScen)
+m = StructuredModel(num_scenarios=numScen)
 
 @variable(m, x[1:N] >= 0)
 @constraint(m, sum(x[i] for i=1:N) >= -1)
 @objective(m, :Min, sum(x[i] for i=1:N))
 
-bl = StructuredModel(parent=m)
+bl = StructuredModel(parent=m, id=1)
 @variable(bl, y[1:N] >= 0)
 @constraint(bl, constr[r=2:(N-1)], x[r] + x[r-1] + y[r] + y[r+1] >= 0)
 @objective(bl, :Min, sum(y[i] for i=1:N))
--- test/runtests.jl
+++ test/runtests.jl
@@ -4,3 +4,6 @@ include("printhook.jl")
 include("benderstest.jl")
 
 include("farmer.jl")
+
+include("examples_smoketest.jl")
+
