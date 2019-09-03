a29e1fc75ab3129f09fd195f7f00c6bef396e695
Benoît Legat <benoit.legat@gmail.com>
2019-09-03 13:12:03 +0200

    Implement printing

--- test/printhook.jl
+++ test/printhook.jl
@@ -5,21 +5,38 @@ using StructJuMP
 @testset "printhook" begin
 
     numScen = 2
-    m = StructuredModel(num_scenarios=numScen)
+    parent = StructuredModel(num_scenarios=numScen)
 
-    @variable(m, 0 <= x <= 1)
-    @variable(m, 0 <= y <= 1)
+    @variable(parent, 0 <= x <= 1)
+    @variable(parent, 0 <= y <= 1)
 
-    @constraint(m, x + y == 1)
-    @objective(m, Min, x*x + y)
+    @constraint(parent, x + y == 1)
+    @objective(parent, Min, x*x + y)
 
     for i in 1:numScen
-        bl = StructuredModel(parent=m, id=i)
+        bl = StructuredModel(parent=parent, id=i)
         @variable(bl, w >= 0)
         @constraint(bl, w - x - y <= 1)
         @objective(bl, Min, w*w + w)
     end
 
-    str = string(m)
-    @test occursin("Child", str)
+    str = string(parent)
+    @test sprint(print, parent) == """
+Min x² + y
+Subject to
+ x + y = 1.0
+"""
+    @test sprint(show, parent) == """
+A JuMP Model
+Minimization problem with:
+Variables: 2
+Objective function type: GenericQuadExpr{Float64,StructJuMP.StructuredVariableRef}
+Constraint: 1
+Names registered in the model: x, y"""
+    @test sprint(show, "text/latex", parent) == """
+\$\$ \\begin{alignat*}{1}\\min\\quad & x^2 + y\\\\
+\\text{Subject to} \\quad & x + y = 1.0\\\\
+\\end{alignat*}
+ \$\$"""
+    #@test occursin("Child", str)
 end
